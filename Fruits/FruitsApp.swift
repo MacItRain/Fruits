e7788e5cb827c31b67e0b29f53dc24b70cfb10fb
ChanYoung <a_b_ab@naver.com>
2023-07-24 17:55:35 +0900

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by 이찬영 on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
